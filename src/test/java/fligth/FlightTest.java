d9117f4885c7cf30fcc3aa74d87225cd76cf65c1
alu0101203942 <alu0101203942@ull.edu.es>
2021-12-18 09:55:49 +0000

    Creando la estructura inicial de las pruebas

---
+++ src/test/java/fligth/FlightTest.java
@@ -0,0 +1,24 @@
+package fligth;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightTest {
+
+    @Test
+    void getFlightNumber() {
+    }
+
+    @Test
+    void getNumberOfPassengers() {
+    }
+
+    @Test
+    void addPassenger() {
+    }
+
+    @Test
+    void removePassenger() {
+    }
+}
---
+++ src/test/java/passengers/PassengerTest.java
@@ -0,0 +1,36 @@
+package passengers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PassengerTest {
+
+    @Test
+    void getIdentifier() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getCountryCode() {
+    }
+
+    @Test
+    void getFlight() {
+    }
+
+    @Test
+    void joinFlight() {
+    }
+
+    @Test
+    void setFlight() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
